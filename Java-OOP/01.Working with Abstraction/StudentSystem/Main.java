f18a5898f82ad2a3c2d77bfd882cdec47d3dd5e7
NikolayKarov <nikolay.karov777@gmail.com>
2023-01-12 11:30:20 +0200

    Add solutions for the Exercises

---
+++ Java-OOP/01.Working with Abstraction/StudentSystem/Main.java
@@ -0,0 +1,14 @@
+package StudentSystem;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StudentSystem system = new StudentSystem();
+        system.start();
+
+    }
+}
+
---
+++ Java-OOP/01.Working with Abstraction/StudentSystem/StudentSystem.java
@@ -0,0 +1,27 @@
+package StudentSystem;
+
+import StudentSystem.Input.Reader;
+
+public class StudentSystem {
+
+    private CommandHandler handler;
+    private boolean isWorking;
+
+    public StudentSystem() {
+        this.handler = new CommandHandler();
+    }
+
+    public void start() {
+        this.isWorking = true;
+        while (isWorking) {
+            String result = handler.handleCommand(Reader.readStringArray("\\s+"));
+
+            if (result != null && !result.equals("Exit")) {
+                System.out.println(result);
+            }
+            isWorking = !"Exit".equals(result );
+        }
+    }
+}
+
+
